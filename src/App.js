c601c65d19c949e8eb0e3682aabbdc2fb3b4ed9a
PanArtur <pankovets.artur1@gmail.com>
2025-08-01 15:07:24 +0300

    creating deploy-configuration and architecture

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+
+export const App = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+};
---
+++ src/components/Contaner/Container.jsx
@@ -0,0 +1,7 @@
+import styles from './Container.module.scss';
+
+export const Container = ({ className, children }) => {
+    return <div className={`${styles.container} ${className}`}>
+      {children}
+    </div>
+}
