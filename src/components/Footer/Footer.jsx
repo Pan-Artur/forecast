0e9e187538051b7249ae32ccce7fbd9c9404680f
DimaVax <126974388+DimaVax@users.noreply.github.com>
2025-08-01 15:48:10 +0200

    sas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,40 @@
+import { Container } from "../Contaner/Container";
+
 export const Footer = () => {
   return (
     <footer>
-      
+      <Container >
+        
+        <svg>
+          <use></use>
+        </svg>
+        <ul className="footerList">
+          <li className="footerItem">Address</li>
+          <li className="footerItem">Svobody str. 35</li>
+          <li className="footerItem">Kyiv</li>
+          <li className="footerItem">Ukraine</li>
+        </ul>
+        <div className="footerContacts">
+          <h3 className="footerSuptitle">Contact us</h3>
+          <ul className="footerContactsList">
+            <li className="footerContactsItem">
+              <svg className="footerItem">
+                <use></use>
+              </svg>
+            </li>
+            <li className="footerContactsItem">
+              <svg className="footerItem">
+                <use></use>
+              </svg>
+            </li>
+            <li className="footerContactsItem">
+              <svg className="footerItem">
+                <use></use>
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </footer>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 export const Header = () => {
   return (
     <header>
-      
+      <div className="headerNavBox">
+        <svg>
+          <use></use>
+        </svg>
+        <nav className="headerNav">
+          <a href="" className="headerNavItem">Who we are</a>
+          <a href="" className="headerNavItem">Contacts</a>
+          <a href="" className="headerNavItem">Menu</a>
+        </nav>
+      </div>
+      <div className="headerLogInBox">
+        <button type="button" className="headerBtn">Sing Up</button>
+        <img src="" alt="Profile Picture" className="headerPP" />
+      </div>
     </header>
   );
 }
